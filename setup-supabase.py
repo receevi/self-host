3430e9ec57f38fd8c6eb4e4263258bef06e5bb2a
Juned Khatri <juned.khatri31@gmail.com>
2024-07-27 16:47:10 +0530

    Update name in supabase docker compose

--- setup-supabase.py
+++ setup-supabase.py
@@ -76,6 +76,7 @@ def setup_docker_compose(app_config):
     if 'KONG_NGINX_PROXY_LUA_SSL_TRUSTED_CERTIFICATE' not in kong_env_vars:
         kong_env_vars['KONG_NGINX_PROXY_LUA_SSL_TRUSTED_CERTIFICATE'] = '/etc/ssl/certs/ca-certificates.crt'
 
+    compose_file_content['name'] = f'{product_name}-supabase'
     product_name = app_config[KEY_PRODUCT_NAME]
     for _, service_obj in compose_file_content['services'].items():
         if product_name not in service_obj['container_name']:
